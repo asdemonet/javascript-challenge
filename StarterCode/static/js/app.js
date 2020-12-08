ef730579292de09c34b0035c19d658a000a3f1e6
asdemonet <62965786+asdemonet@users.noreply.github.com>
2020-12-08 11:51:28 -0500

    start app

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var datetime = data.datetime;
+
+
+var filter = d3.select("#filters");
+var button = d3.select("#filter-btn");
+
+button.on("click", runEnter);
+
+function runEnter() {
+    d3.event.preventDefault();
+
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+    console.log(filteredData);
+}
